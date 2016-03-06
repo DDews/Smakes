5c7058a558156375038996ea84acfb7b957daec7
ninjapretzel <ninjapretzel@yahoo.com>
2016-03-06 16:53:02 -0700

    Makes shit happen automatically during games

--- client/global/helpers.js
+++ client/global/helpers.js
@@ -30,11 +30,14 @@ Handlebars.registerHelper('currentUserWallet', () => {
 	return wallet.toPairRay();
 })
 
-function div(clas, content, style) { 
-	if (style) {
-		return '<div class="' + clas + '" style="' + style+ '">' + content + '</div>'
-	}
-	return '<div class="' + clas + '">' + content + '</div>'
+function div(clas, content, style, id) { 
+	var sty = " ";
+	var i = " ";
+	var c = ' class="' + clas + '"';
+	
+	if (style) { sty += 'style="' + style+ '"'; }
+	if (id) { i += 'id="' + id + '"'; }
+	return '<div' + c + i + sty + '">' + content + '</div>';
 }
 function span(clas, content) { 
 	return '<span class="' + clas + '">' + content + '</span>' 
@@ -51,13 +54,13 @@ function unCamelCase(string) {
 
 Handlebars.registerHelper('unCamelCase', unCamelCase)
 
-Handlebars.registerHelper('healthbar', (val, max, color) => {
+Handlebars.registerHelper('healthbar', (val, max, color, fillId) => {
 	if (!max) { max = 1.0; }
 	var fill = Math.floor(val / max * 100);
 	if (!color) { color = "green"; }
 	
 	return div(progressBack + color, 
-			   div(progressFront + color, "", "width: " + fill + "%")
+			   div(progressFront + color, "", "width: " + fill + "%", fillId)
 			  );
 	
 })
@@ -84,7 +87,7 @@ function vitalContent(unit, vital, color, mvital) {
 	var val = unit.has(vital) ? unit[vital] : mval * .5;
 	
 	var str = div("small", vital + ": " + Math.floor(val) + " / " + Math.floor(mval));
-	str += healthbar(val, mval, color);
+	str += healthbar(val, mval, color, unit._id+vital);
 	
 	return str;
 }
@@ -94,20 +97,61 @@ function vitalContentNoText(unit, vital, color, mvital) {
 	var mval = unit[mvital];
 	var val = unit.has(vital) ? unit[vital] : mval * .5;
 	
-	return healthbar(val, mval, color);
+	return healthbar(val, mval, color, unit._id+vital);
 }
 
-Handlebars.registerHelper('unitCard', (_id) => {
-	var unit = Unitinfo.find(_id).fetch()[0];
+function header(unit) {
+	return "lv. " + unit.level + " " + unit.race + " " + unit.job + " (" + unit.team + ")";
+}
+
+Handlebars.registerHelper('combatCard', (_id) => {
+	var unit = Unitinfo.findOne(_id);
 	console.log(unit);
-	console.log("unit: " + _id);
+	if (!unit) { return _id; }
 	
-	console.log(Unitinfo.find().count())
+	//console.log("unit: " + _id);
+	//console.log(Unitinfo.find().count())
+	
+	var card = '<div class="col s12 m6 l3 card blue-grey darken-4" id="' + _id + '">'
+	card += '<div class="card-content white-text">'
+	card += '<span class="card-title">' + unit.name + '</span>'
+	card += br + header(unit) + br + br;
+	
+	if (unit.team === 'player') {
+		card += vitalContent(unit, "hp", "green")
+		card += vitalContent(unit, "mp", "blue")
+		card += vitalContent(unit, "sp", "red")
+		card += vitalContent(unit, "exp", "purple", "tnl")
+	} else {
+		card += vitalContent(unit, "hp", "green")
+		card += vitalContentNoText(unit, "mp", "blue")
+		card += vitalContentNoText(unit, "sp", "red")
+		
+	}
+	
+	card += vitalContentNoText(unit, "timer", "cyan", "timeout")
+	
+	card += '</div>'
+	card += '<div class="card-action">'
+	card += "Abilities" + br + "[Attack]"
+	card += '</div>'
+	
+	card += '</div>'
+	
+	return card;
+})
+
+Handlebars.registerHelper('menuCard', (_id) => {
+	var unit = Unitinfo.find(_id).fetch()[0];
+	//console.log(unit);
+	//console.log("unit: " + _id);
+	
+	//console.log(Unitinfo.find().count())
 	
 	var card = '<div class="col s12 m6 l3 card blue-grey darken-4">'
 	card += '<div class="card-content white-text">'
 	card += '<span class="card-title">' + unit.name + '</span>'
-	card += br + "lv. " + unit.level + " " + unit.job + " (" + unit.team + ")" + br + br;
+	card += br + header(unit) + br + br;
 	
 	card += vitalContent(unit, "hp", "green")
 	card += vitalContent(unit, "mp", "blue")
@@ -118,6 +162,7 @@ Handlebars.registerHelper('unitCard', (_id) => {
 	card += '</div>'
 	
 	card += '<div class="card-action">'
+	card += "Equip Info"
 	
 	card += '</div>'
 	
