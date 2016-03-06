99455bd6741905139a4336b98f142b825fdc80ee
ninjapretzel <ninjapretzel@yahoo.com>
2016-03-06 15:23:37 -0700

    Splits Units, Combat, and Game infos into their own databases.

--- server/game.js
+++ server/game.js
@@ -3,12 +3,33 @@ Meteor.publish("gamedata", function() {
 	console.log(this.userId);
 	var user = Meteor.users.findOne(this.userId)
 	var username = user && user.username
-	console.log(username);
+	console.log("gamedata for : " +username);
 	
 	return Gameinfo.find( { username: username } );
 	
 });
 
+
+Meteor.publish("unitinfo", function() {
+	console.log(this.userId);
+	var user = Meteor.users.findOne(this.userId)
+	var username = user && user.username
+	console.log("unitinfo for : " +username);
+	
+	return Unitinfo.find( { username: username } );
+	
+});
+
+
+Meteor.publish("combatinfo", function() {
+	console.log(this.userId);
+	var user = Meteor.users.findOne(this.userId)
+	var username = user && user.username
+	console.log("combatinfo for : " + username);
+	
+	return Combatinfo.find( { username: username } );
+	
+});
 
 Meteor.methods({
 	newGame: (data) => {
@@ -22,21 +43,24 @@ Meteor.methods({
 		}
 		
 		var unit = new Unit();
+		unit.username = username;
 		unit.name = data.name;
 		unit.job = data.job;
 		
+		dbupdate(unit);
+		
 		console.log(unit);
 		
-		var gameData = {
+		var gamedata = {
 			username: username,
-			units: [unit],
+			units: [unit._id],
 			items: [],
 			combat: null,
 			kills: 0,
-		}
-			
-		console.log(gameData);
-		Gameinfo.insert(gameData);
+		};
+		dbinsert("Gameinfo", gamedata)
+		
+		console.log(gamedata);
 		
 	},
 	
@@ -44,9 +68,41 @@ Meteor.methods({
 		console.log("combat is starting!");	
 		var username = Meteor.user() && Meteor.user().username;
 		if (!username) { throw new Meteor.Error(422, "Error: You must be logged in"); }
+		console.log("user found!");	
+		var gamedata = Gameinfo.findOne({username: username});
+		if (!gamedata) { throw new Meteor.Error(422, "Error: You must have a game started"); }
+		console.log("gamedata found!");	
+		var userinfo = Userinfo.findOne({username: username});
+		console.log("userinfo found!");	
 		
-		var mob = Monster("goblin", 1, 0)
-		console.log(mob);
+		var region = data.region;
+		var regionData = areaData[region];
+		console.log("region found!");	
+		if (!regionData) { throw new Meteor.Error(422, "Error: Unknown region " + region); }
+		var enemies = regionData.enemies;
+		
+		
+		var units = []
+		
+		gamedata.units.each(
+			(id) => { 
+				var u = dbget("Unitinfo", id);
+				u.fullHeal(); units.push(u); 
+		} );
+		console.log("units added!");	
+		var size = 1 + Random.range(regionData.min, regionData.max) * .5;
+		size.times((s) => { units.push(Monster(enemies.choose(), 1, 0)); })
+		console.log("monsters generated!");
+		
+		var combat = new Combat(units);
+		
+		gamedata.combat = combat;
+		combat.username = username;
+		console.log(combat);
+		dbupdate(gamedata);
+		
+		console.log("saved!");	
+		
 		
 		
 	},
