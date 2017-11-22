9bde6bb911552ce71f141cab11ea380fec448e7a
Dan Dews <danielricharddews@gmail.com>
2017-11-22 14:29:25 -0700

    Trying to make a single room

--- client/game/game.js
+++ client/game/game.js
@@ -1,66 +1,148 @@
-var SIZE = 50;
+var SIZE = 100.0;
 var _moving = 'right';
 var _keys = new Set();
 var _started = false;
+var _speed = "normal";
 var _timeout;
+var _playing = false;
 var _stop = function (key) {
-	if (_started) {
-		_keys.delete(key);
-		if (key == "ShiftLeft" || key == "ShiftRight") Meteor.call("normalSpeed");
-		else if (key == 'Space') Meteor.call("normalSpeed");
-		console.log(key);
-	}
+	keyboard.stop();
 };
-var _move = function (key) {
-	if (_started) {
-		_keys.add(key);
-		switch (key) {
-			case 'KeyA':
-			case 'ArrowLeft':
-				Meteor.call("moveLeft");
-				break;
-			case 'KeyD':
-			case 'ArrowRight':
-				Meteor.call("moveRight");
-				break;
-			case 'KeyW':
-			case 'ArrowUp':
-				Meteor.call("moveUp");
-				break;
-			case 'KeyS':
-			case 'ArrowDown':
-				Meteor.call("moveDown");
-				break;
-			case 'ShiftLeft':
-			case 'ShiftRight':
-				Meteor.call("fastSpeed");
-				break;
-			case 'Space':
-				Meteor.call("slowSpeed");
+function mainLoop (time) {
+	if (_keys.ArrowUp || _keys.KeyW) {
+		if (_moving != "up") {
+			Meteor.call("moveUp");
+			_moving = "up";
 		}
 	}
-};
+	if (_keys.ArrowDown || _keys.KeyS) {
+		if (_moving != "down") {
+			Meteor.call("moveDown");
+			_moving = "down";
+		}
+	}
+	if (_keys.ArrowLeft || _keys.KeyA) {
+		if (_moving != "left") {
+			Meteor.call("moveLeft");
+			_moving = "left";
+		}
+	}
+	if (_keys.ArrowRight || _keys.KeyD) {
+		if (_moving != "right") {
+			Meteor.call("moveRight");
+			_moving = "right";
+		}
+	}
+	if (_keys.Space) {
+		if (_speed != "slow") {
+			Meteor.call("slowSpeed");
+			_speed = "slow";
+		}
+	}
+	else if (_keys.ShiftLeft || _keys.ShiftRight) {
+		if (_speed != "fast") {
+			Meteor.call("fastSpeed");
+			_speed = "fast";
+		}
+	} else if (_speed != "normal") {
+		Meteor.call("normalSpeed");
+		_speed = "normal";
+	}
+	requestAnimationFrame(mainLoop);
+}
+const keyboard = (() => {
+    var active = false;
+    const keys = {
+        ArrowLeft : false,  // only add keys you are interested in
+        ArrowRight : false,
+				ArrowDown: false,
+				ArrowUp: false,
+				KeyW: false,
+				KeyA: false,
+				KeyS: false,
+				KeyD: false,
+        Space : false,
+				ShiftLeft: false,
+				ShiftRight: false,
+        anyKey : false,
+    };
+    // map keyCodes to named code.
+    const keyCodeMap = {
+         k37 : "ArrowLeft",
+         k39 : "ArrowRight",
+         k32 : "Space",
+    };
+    function keyEvents (e) {
+        var code = e.code;
+        if (! code) { // if no support for code
+            code = keyCodeMap["k" + e.keyCode];
+        }
+        if (keys[code] !== undefined) {
+            keys[code] = e.type === "keydown";
+            e.preventDefault();
+        }
+        keys.anyKey = true;
+    }
+    const API = {
+        start () {
+            if (!active) {
+                addEventListener("keyup", keyEvents);
+                addEventListener("keydown", keyEvents);
+                active = true;
+            }
+            return keys;
+        },
+        stop () {
+            if (active) {
+                removeEventListener("keyup", keyEvents);
+                removeEventListener("keydown", keyEvents);
+                active = false;
+             }
+        }
+    }
+    return API;
+})();
+var drawApple = function () {
+	var c = document.getElementById("canvas");
+	var ctx = c.getContext("2d");
+	var w = c.width;
+	var h = c.height;
+	var px = w / SIZE;
+	var py = h / SIZE;
+	apple = Apple.findOne({});
+	if (apple) {
+		ctx.beginPath();
+		ctx.fillStyle = "#FFFFFF";
+		ctx.rect(Math.floor(apple.x * px),Math.floor(apple.y * py), Math.ceil(px), Math.ceil(py));
+		ctx.lineWidth=px;
+		ctx.fill();
+	}
+}
 var _first = function () {
+	_keys = keyboard.start();
+	_playing = true;
+	requestAnimationFrame(mainLoop);
 	var c = document.getElementById("canvas");
 	if (c) {
+		c.width = window.innerWidth;
+		c.height = window.innerHeight;
 		var user = Meteor.user();
 		var username = user && user.username;
 		var ctx = c.getContext("2d");
 		var w = c.width;
 		var h = c.height;
-		var px = Math.floor(w / SIZE);
-		var py = Math.floor(h / SIZE);
+		var px = w / SIZE;
+		var py = h / SIZE;
 		ctx.clearRect(0, 0, w, h);
 		Pixels.find({username: user.username}).forEach(smake => {
 			ctx.beginPath();
 			ctx.fillStyle = smake.color;
-			ctx.rect(smake.x * px,smake.y * py, px, py);
+			ctx.rect(Math.floor(smake.x * px),Math.floor(smake.y * py), Math.ceil(px), Math.ceil(py));
 			ctx.lineWidth=px;
 			ctx.fill();
 		});
+		drawApple();
 	}
-	document.addEventListener('keydown', function (e) { if (e.code == 'AltLeft' || e.code == 'AltRight') e.preventDefault(); _move(e.code); });
-	document.addEventListener('keyup', function (e) { if (_timeout) clearTimeout(_timeout); _timeout = setTimeout( function() { if (e.code == 'AltLeft' || e.code == 'AltRight') e.preventDefault(); _stop(e.code); }, 150) });
 	_started = true;
 };
 var _draw = function () {
@@ -72,28 +154,29 @@ var _draw = function () {
 		var ctx = c.getContext("2d");
 		var w = c.width;
 		var h = c.height;
-		var px = Math.floor(w / SIZE);
-		var py = Math.floor(h / SIZE);
-		Smakes.find({username: user.username}).forEach(smake => {
+		var px = w / SIZE;
+		var py = h / SIZE;
+		Smakes.find().forEach(smake => {
 			ctx.beginPath();
 			ctx.fillStyle = smake.color;
 			ctx.lineWidth=px;
-			ctx.rect(smake.x * px,smake.y * py, px, py);
+			ctx.rect(Math.floor(smake.x * px),Math.floor(smake.y * py), Math.ceil(px), Math.ceil(py));
 			ctx.fill();
 			ctx.closePath();
 		});
-		var mostRecent = {};
-		DeadPixels.find({username: user.username}).forEach(smake => {
-			ctx.clearRect(smake.x * px, smake.y * py, px, py);
-			if (mostRecent[smake.smake]) {
-				if (smake.createdAt > mostRecent[smake.smake]) mostRecent[smake.smake] = smake.createdAt;
-			} else {
-				mostRecent[smake.smake] = smake.createdAt;
+		var smakes = {};
+		var mostRecent = 0;
+		DeadPixels.find().forEach(smake => {
+			ctx.clearRect(Math.floor(smake.x * px),Math.floor(smake.y * py), Math.ceil(px), Math.ceil(py));
+			if (!(smake.username in smakes)) {
+				smakes[smake.username] = smake.createdAt;
 			}
+			if (smake.createdAt > mostRecent) mostRecent = smake.createdAt;
 		});
-		for (var smake in mostRecent) {
-			Meteor.call("removePixel",smake, mostRecent[smake]);
+		for (var smake in smakes) {
+			Meteor.call("removePixel",smake, mostRecent);
 		}
+		drawApple();
 	}
 };
 Tracker.autorun(function () {
--- server/game.js
+++ server/game.js
@@ -1,11 +1,13 @@
 var interval;
-var SIZE = 50;
+var SIZE = 100;
 var SPEED = 100; // speed in ms
 var Heads = {};
 var Tails = {};
 var Pix = {};
 var Snakes = {};
+var colors = ["#00FF00","#0000FF","FFFF00","00FFFF","#FFA500"];
 var players = 0;
+var apple = {x: 0, y: 0};
 function makeArray(w, h, val) {
     var arr = [];
     for(i = 0; i < h; i++) {
@@ -16,317 +18,230 @@ function makeArray(w, h, val) {
     }
     return arr;
 }
-//////////////////////////////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////////////////////
-//Published data for game
-
-Meteor.publish("gamedata", function() {
-	//console.log("User id for gamedata: " + this.userId);
-	var user = Meteor.users.findOne(this.userId)
-	var username = user && user.username
-	console.log("gamedatas for " + username + " : " + Gameinfo.find({username: username}).count() );
-
-	return Gameinfo.find( { username: username } );
-
-});
-
-Meteor.publish("allInventory", function() {
-	return Iteminfo.find({username: "<middleman>"});
-})
-Meteor.publish("inventory", function() {
-	var user = Meteor.users.findOne(this.userId)
-	var username = user && user.username
-	console.log("items for " + username + " : " + Iteminfo.find({username: username}).count() );
-
-	return Iteminfo.find( { username: username } );
-
-})
-
-Meteor.publish("shopitems", function() {
-	return Iteminfo.find( { username: "<shop>"} )
-})
-
-
-Meteor.publish("unitinfo", function() {
-	//console.log("User id for unitinfo: " + this.userId);
-	var user = Meteor.users.findOne(this.userId)
-	var username = user && user.username
-	console.log("unitinfos for " + username + " : " + Unitinfo.find({username: username}).count() );
-
-	return Unitinfo.find( { username: username } );
-
-});
-
-Meteor.publish("allunits", function() {
-	return Unitinfo.find();
-})
-
-Meteor.publish("combatinfo", function() {
-	//console.log("User id for combatinfo: " + this.userId);
-	var user = Meteor.users.findOne(this.userId)
-	var username = user && user.username
-	console.log("combatinfos for " + username + " : " + Combatinfo.find({username: username}).count() );
-
-	return Combatinfo.find( { username: username } );
-
-});
-
-
-//////////////////////////////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////////////////////
-//Messages that can be sent to the server by clients for game logic.
-
 Meteor.methods({
 
 	//Removes the current user's game data.
 	stopGame: () => {
 		var username = Meteor.user() && Meteor.user().username;
 		if (!username) { throw new Meteor.Error(422, "Error: You must be logged in"); }
-    players--;
-		Smakes.remove({username: username});
-		Pixels.remove({username: username});
-		DeadPixels.remove({username: username});
-		Meteor.clearInterval(interval);
+    delete Heads[username];
+    Smakes.remove({username: username});
+    players = Smakes.find().fetch().length;
+    if (players <= 1) {
+      Smakes.remove({});
+      Pixels.remove({});
+      DeadPixels.remove({});
+      Meteor.clearInterval(interval);
+    }
+    else {
+  		Smakes.remove({username: username});
+  		Pixels.remove({username: username});
+  		DeadPixels.remove({username: username});
+    }
 	},
 	newGame: () => {
 		var username = Meteor.user() && Meteor.user().username;
 		if (!username) { throw new Meteor.Error(422, "Error: You must be logged in"); }
+    if (players >= 5) { throw new Meteor.Error(422, "Error: too many players playing!"); }
 		Smakes.remove({username: username});
 		Pixels.remove({username: username});
 		DeadPixels.remove({username: username});
-		smakes = Smakes.find({username: username}).fetch();
-		pixels = Pixels.find({username: username}).fetch();
-		if (smakes.length > 0) {
-			smakes.each((d) => { Smakes.remove(d._id); });
-		}
-		if (pixels.length > 0) {
-			pixels.each((d) => { Pixels.remove(d._id); });
-		}
-		Smakes.insert({username: username, color: "#00FF00", speed: 1, tick: 0, length: 10, dead: false, bot: false, x: 0, y: 0, dx: 1, dy: 0, size: 1, createdAt: +new Date()});
-		Smakes.insert({username: username, color: "#FF0000", speed: 1, tick: 0, length: 10, dead: false, bot: true, x: SIZE - 1, y: SIZE - 1, dx: -1, dy: 0, size: 1, createdAt: +new Date()});
-    var smake = Smakes.findOne({username: username, bot: {$ne: true}});
-		var opponent = Smakes.findOne({username:username, bot: {$ne: false}});
+    players = Smakes.find().fetch().length;
+		Smakes.insert({username: username, color: colors[players], speed: 1, tick: 0, length: 10, dead: false, bot: false, x: 0, y: 0, dx: 1, dy: 0, size: 1, createdAt: +new Date()});
+		var smake = Smakes.findOne({username: username, bot: {$ne: true}});
     Pixels.insert({username: username, smake: smake._id, color: "#00FF00", speed: 1, tick: 0, length: 10, dead: false, bot: false, x: 0, y: 0, dx: 1, dy: 0, size: 1, createdAt: +new Date()});
-    Pixels.insert({username: username, smake: opponent._id, color: "#FF0000", speed: 1, tick: 0, length: 10, dead: false, bot: true, x: SIZE - 1, y: SIZE - 1, dx: -1, dy: 0, size: 1, createdAt: +new Date()});
-
-		Heads[username] = [
-			smake,
-			opponent
-		];
-		Pix[username] = makeArray(100,100,undefined);
-		Pix[username][smake.x][smake.y] = 0;
-		Pix[username][opponent.x][opponent.y] = 1;
-		Snakes[username] = makeArray(2,1,undefined);
-		Snakes[username][0] = [{x: smake.x, y: smake.y}];
-		Snakes[username][1] = [{x: opponent.x, y: opponent.y}];
+    Heads[username] = smake;
+    if (!('bot' in Heads)) {
+      apple = {x: Math.round(Math.random() * SIZE), y: Math.round(Math.random() * SIZE)};
+      Apple.remove({});
+      Apple.insert(apple);
+      apple = Apple.findOne({});
+      Smakes.insert({username: 'bot', color: "#FF0000", speed: 1, tick: 0, length: 10, dead: false, bot: true, x: SIZE - 1, y: SIZE - 1, dx: -1, dy: 0, size: 1, createdAt: +new Date()});
+      var opponent = Smakes.findOne({username:'bot'});
+      Pixels.insert({username: 'bot', smake: opponent._id, color: "#FF0000", speed: 1, tick: 0, length: 10, dead: false, bot: true, x: SIZE - 1, y: SIZE - 1, dx: -1, dy: 0, size: 1, createdAt: +new Date()});
+      Pix = makeArray(100,100,undefined);
+  		Pix[smake.x][smake.y] = username;
+  		Pix[opponent.x][opponent.y] = username;
+      Snakes = {};
+      Snakes[username] = [{x: smake.x, y: smake.y}];
+      Snakes['bot'] = [{x: opponent.x, y: opponent.y}];
+      Heads['bot'] = opponent;
+    }
+    else {
+      Snakes[username] = [{x: smake.x, y: smake.y}];
+      Pix[smake.x][smake.y] = username;
+    }
     if (players == 0) {
+      if (typeof interval != undefined) Meteor.clearTimeout(interval);
   		interval = Meteor.setInterval(() => {
-  				Meteor.call("tick",username)
+  				Meteor.call("tick");
   			},SPEED);
     }
-    players++;
+    players = Object.keys(Heads).length;
 	},
 	tick: () => {
+    var updateDead = false;
     var PixelsRaw = Pixels.rawCollection().initializeUnorderedBulkOp();
     PixelsRaw.executeAsync = Meteor.wrapAsync(PixelsRaw.execute);
 
     var DeadPixelsRaw = DeadPixels.rawCollection().initializeUnorderedBulkOp();
     DeadPixelsRaw.executeAsync = Meteor.wrapAsync(DeadPixelsRaw.execute);
-
-    var updateDead = false;
-
-    for (var username in Heads) {
-  		for (i = 0; i < Heads[username].length; i++) {
-        var dying = false;
-  			var d = Heads[username][i];
-  			var dx = +new Date();
-  			dx -= d.createdAt;
-  			dx = (SPEED * d.speed) / dx;
-  			d.tick++;
-  			if (d.tick >= d.speed) {
-  				d.tick = 0;
-          if (d.speed == 0.5) doubleSpeed = 2;
-          else doubleSpeed = 1;
-          for (y = 0; y < doubleSpeed; y++) {
-            d.size += 1;
-    				var newx = d.x + Math.round(d.dx * dx);
-    				while (newx >= SIZE) newx -= SIZE;
-    				while (newx < 0) newx += SIZE;
-    				var newy = d.y + Math.round(d.dy * dx);
-    				while (newy >= SIZE) newy -= SIZE;
-    				while (newy < 0) newy += SIZE;
-    				var n = {
-    					smake: d._id,
-    					username: username,
-    					color: d.color,
-    					speed: d.speed,
-    					bot: d.bot,
-    					x: 0,
-    					y: 0,
-    					dx: +d.dx,
-    					dy: +d.dy,
-    					createdAt: +new Date()
-    				};
-    				d.x += d.dx;
-    				if (d.x >= SIZE) d.x -= SIZE;
-    				else if (d.x < 0) d.x += SIZE;
-    				d.y += d.dy;
-    				if (d.y >= SIZE) d.y -= SIZE;
-    				else if (d.y < 0) d.y += SIZE;
-    				var hit = Pix[username][d.x][d.y];
-    				if (hit != undefined) {
-              dying = true;
-    					for (k = 0; k < Snakes[username][i].length; k++) {
-    						var died = Snakes[username][i][k];
-    						var j = {
-    							username: username,
-    							smake: d._id,
-    							x: died.x,
-    							y: died.y,
-    							createdAt: +new Date()
-    						};
-    						Pix[username][died.x][died.y] = undefined;
-    					  DeadPixelsRaw.insert(j);
-                updateDead = true;
-    					}
-              PixelsRaw.find({username: username, smake: d._id}).removeOne();
-              DeadPixelsRaw.insert(n);
-    					d.x = Math.floor(Math.random() * SIZE);
-    					d.y = Math.floor(Math.random() * SIZE);
-    					d.dx = 1;
-    					d.dy = 0;
-    					d.length = 10;
-    					d.size = 1;
-              console.log(i + ": " + d.x + ", " + d.y);
-    					Snakes[username][i] = [{x: d.x, y: d.y}];
-    				}
-    				n.x = d.x;
-    				n.y = d.y;
-    				Pix[username][d.x][d.y] = i;
-            if (typeof Snakes[username][i] == undefined) Snakes[username][i] = [];
-    				Snakes[username][i].push({x: d.x,y: d.y});
-    				d.createdAt = +new Date();
-    				n.createdAt = d.createdAt;
-    				if (d.size > d.length) {
-    					var pos = Snakes[username][i][0];
-    					pixel = Pixels.findOne({username: username, smake: d._id, x: pos.x, y: pos.y},{sort: {createdAt: 1}});
-    					if (pixel) {
-    						PixelsRaw.find({_id: pixel._id}).removeOne();
-    						delete pixel._id;
-                delete pixel._collection;
-    						DeadPixelsRaw.insert(pixel);
-                updateDead = true;
-    					}
-    					Pix[username][pos.x][pos.y] = undefined;
-              if (typeof Snakes[username][i] == undefined) Snakes[username][i] = [];
-    					if (Snakes[username][i].length > 1) Snakes[username][i].shift();
-    					if (typeof Snakes[username][i] == undefined) Snakes[username][i] = [{x: d.x, y: d.y}];
-    					d.size--;
-    				}
-    				Smakes.update(d._id, {$set: d});
-    				Heads[username][i] = d;
-    				PixelsRaw.insert(n);
-            if (dying) DeadPixelsRaw.insert(n);
-    			}
-    			Heads[username][i] = d;
-        }
-  		}
-    }
+		for (var username in Heads) {
+      var d = Heads[username];
+      var dying = false;
+			d.tick++;
+			if (d.tick >= d.speed) {
+        var doubleSpeed = 1;
+				d.tick = 0;
+        if (d.speed == 0.5) doubleSpeed = 2;
+        else doubleSpeed = 1;
+        for (y = 0; y < doubleSpeed; y++) {
+          d.size += 1;
+  				var n = {
+  					username: username,
+  					color: d.color,
+  					speed: d.speed,
+  					bot: d.bot,
+  					x: 0,
+  					y: 0,
+  					dx: +d.dx,
+  					dy: +d.dy,
+  					createdAt: +new Date()
+  				};
+  				d.x += d.dx;
+  				if (d.x >= SIZE) d.x -= SIZE;
+  				else if (d.x < 0) d.x += SIZE;
+  				d.y += d.dy;
+  				if (d.y >= SIZE) d.y -= SIZE;
+  				else if (d.y < 0) d.y += SIZE;
+  				var hit = Pix[d.x][d.y];
+  				if (typeof hit == "string") {
+            dying = true;
+  					for (k = 0; k < Snakes[username].length; k++) {
+  						var died = Snakes[username][k];
+  						var j = {
+  							username: username,
+  							smake: d._id,
+  							x: died.x,
+  							y: died.y,
+  							createdAt: +new Date()
+  						};
+  						Pix[died.x][died.y] = 0;
+  					  DeadPixelsRaw.insert(j);
+              updateDead = true;
+  					}
+            PixelsRaw.find({username: username}).remove();
+            DeadPixelsRaw.insert(n);
+  					d.x = Math.floor(Math.random() * SIZE);
+  					d.y = Math.floor(Math.random() * SIZE);
+  					d.dx = 1;
+  					d.dy = 0;
+  					d.length = 10;
+  					d.size = 1;
+  					Snakes[username] = [{x: d.x, y: d.y}];
+  				} else if (Apple.find({x: d.x, y: d.y}).fetch().length > 0) {
+            apple.x = Math.round(Math.random() * SIZE);
+            apple.y = Math.round(Math.random() * SIZE);
+            Apple.update(apple._id, {$set: apple});
+            d.length += 10;
+          }
+  				n.x = d.x;
+  				n.y = d.y;
+  				Pix[d.x][d.y] = username;
+          if (typeof Snakes[username] == undefined) Snakes[username] = [];
+  				Snakes[username].push({x: d.x,y: d.y});
+  				d.createdAt = +new Date();
+  				n.createdAt = d.createdAt;
+  				if (d.size > d.length) {
+  					var pos = Snakes[username][0];
+						PixelsRaw.find({username: username},{sort: {createdAt: 1}}).removeOne();
+						DeadPixelsRaw.insert({
+              username: username,
+              x: pos.x,
+              y: pos.y,
+              createdAt: +new Date()
+            });
+            updateDead = true;
+  					Pix[pos.x][pos.y] = 0;
+            if (typeof Snakes[username] == undefined) Snakes[username] = [{x: d.x, y: d.y}];
+  					if (Snakes[username].length > 1) Snakes[username].shift();
+  					if (typeof Snakes[username] == undefined) Snakes[username] = [{x: d.x, y: d.y}];
+  					d.size--;
+  				}
+  				Smakes.update(d._id, {$set: d});
+  				PixelsRaw.insert(n);
+          if (dying) {
+            DeadPixelsRaw.insert(n);
+            updateDead = true;
+          }
+  			}
+      }
+      Heads[username] = d;
+		}
     PixelsRaw.executeAsync();
     if (updateDead) DeadPixelsRaw.executeAsync();
 	},
 	removePixel: (id, time) => {
-		DeadPixels.remove({smake: id, createdAt: {$lte: time}});
+    var DeadPixelsRaw = DeadPixels.rawCollection().initializeUnorderedBulkOp();
+    DeadPixelsRaw.executeAsync = Meteor.wrapAsync(DeadPixelsRaw.execute);
+    DeadPixelsRaw.find({username: id, createdAt: {$lte: time}}).remove();
+		DeadPixelsRaw.executeAsync();
 	},
 	moveUp: () => {
 		var username = Meteor.user() && Meteor.user().username;
 		if (!username) { throw new Meteor.Error(422, "Error: You must be logged in"); }
-		if (Heads[username][0].dy == 1) return;
-		Heads[username][0].dx = 0;
-		Heads[username][0].dy = -1;
-		smake = Smakes.findOne({username: username, bot: {$ne: true}});
-    if (smake) {
-  		smake.dx = 0;
-  		smake.dy = -1;
-  		Smakes.update(smake._id,{$set: smake});
-    }
+    if (!(username in Heads)) return;
+		if (Heads[username].dy == 1) return;
+    Heads[username].dx = 0;
+		Heads[username].dy = -1;
 	},
 	moveDown: () => {
 		var username = Meteor.user() && Meteor.user().username;
 		if (!username) { throw new Meteor.Error(422, "Error: You must be logged in"); }
-		if (Heads[username][0].dy == -1) return;
-		smake = Smakes.findOne({username: username, bot: {$ne: true}});
-    if (smake) {
-  		Heads[username][0].dx = 0;
-  		Heads[username][0].dy = 1;
-  		smake.dx = 0;
-  		smake.dy = 1;
-  		Smakes.update(smake._id,{$set: smake});
-    }
+    if (!(username in Heads)) return;
+		if (Heads[username].dy == -1) return;
+		Heads[username].dx = 0;
+		Heads[username].dy = 1;
 	},
 	moveLeft: () => {
 		var username = Meteor.user() && Meteor.user().username;
 		if (!username) { throw new Meteor.Error(422, "Error: You must be logged in"); }
-		if (Heads[username][0].dx == 1) return;
-		smake = Smakes.findOne({username: username, bot: {$ne: true}});
-    if (smake) {
-  		Heads[username][0].dx = -1;
-  		Heads[username][0].dy = 0;
-  		smake.dx = -1;
-  		smake.dy = 0;
-  		Smakes.update(smake._id,{$set: smake});
-    }
+    if (!(username in Heads)) return;
+		if (Heads[username].dx == 1) return;
+		Heads[username].dx = -1;
+		Heads[username].dy = 0;
 	},
 	moveRight: () => {
 		var username = Meteor.user() && Meteor.user().username;
 		if (!username) { throw new Meteor.Error(422, "Error: You must be logged in"); }
-		if (Heads[username][0].dx == -1) return;
-		smake = Smakes.findOne({username: username, bot: {$ne: true}});
-    if (smake) {
-  		Heads[username][0].dx = 1;
-  		Heads[username][0].dy = 0;
-  		smake.dx = 1;
-  		smake.dy = 0;
-  		Smakes.update(smake._id,{$set: smake});
-    }
+    if (!(username in Heads)) return;
+		if (Heads[username].dx == -1) return;
+		Heads[username].dx = 1;
+		Heads[username].dy = 0;
 	},
 	normalSpeed: () => {
 		var username = Meteor.user() && Meteor.user().username;
 		if (!username) { throw new Meteor.Error(422, "Error: You must be logged in"); }
-		smake = Smakes.findOne({username: username, bot: {$ne: true}});
-    if (smake) {
-  		Heads[username][0].speed = 1;
-  		if (smake.speed != 1) smake.speed = 1;
-  		Smakes.update(smake._id,{$set: smake});
-    }
+    if (!(username in Heads)) return;
+  	Heads[username].speed = 1;
 	},
 	slowSpeed: () => {
 		var username = Meteor.user() && Meteor.user().username;
 		if (!username) { throw new Meteor.Error(422, "Error: You must be logged in"); }
-		smake = Smakes.findOne({username: username, bot: {$ne: true}});
-    if (smake) {
-  		Heads[username][0].speed = 3;
-  		if (smake.speed != 3) smake.speed = 3;
-  		Smakes.update(smake._id,{$set: smake});
-    }
+    if (!(username in Heads)) return;
+		Heads[username].speed = 3;
 	},
 	fastSpeed: () => {
 		var username = Meteor.user() && Meteor.user().username;
 		if (!username) { throw new Meteor.Error(422, "Error: You must be logged in"); }
-		smake = Smakes.findOne({username: username, bot: {$ne: true}});
-    if (smake) {
-  		Heads[username][0].speed = 0.5;
-  		if (smake.speed != 0.5) smake.speed = 0.5;
-  		Smakes.update(smake._id,{$set: smake});
-    }
+    if (!(username in Heads)) return;
+    Heads[username].speed = 0.5;
 	},
 	purgeSmakes: () => {
-		delete Pix[username];
-		delete Heads[username];
-		delete Tails[username];
-		delete Snakes[username];
+		Pix = [];
+		Heads = {};
+		Snakes = {};
+    players = 0;
 		Pixels.remove({});
 		Smakes.remove({});
 		DeadPixels.remove({});
