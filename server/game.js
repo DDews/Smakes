bbb20ea5de6c250f0651e96eb2203f7448285a76
Dan Dews <danielricharddews@gmail.com>
2017-11-22 09:57:35 -0700

    Game running on single interval

--- server/game.js
+++ server/game.js
@@ -1,10 +1,11 @@
-var intervals = [];
+var interval;
 var SIZE = 50;
 var SPEED = 100; // speed in ms
 var Heads = {};
 var Tails = {};
 var Pix = {};
 var Snakes = {};
+var players = 0;
 function makeArray(w, h, val) {
     var arr = [];
     for(i = 0; i < h; i++) {
@@ -83,16 +84,11 @@ Meteor.methods({
 	stopGame: () => {
 		var username = Meteor.user() && Meteor.user().username;
 		if (!username) { throw new Meteor.Error(422, "Error: You must be logged in"); }
+    players--;
 		Smakes.remove({username: username});
 		Pixels.remove({username: username});
 		DeadPixels.remove({username: username});
-		for (i = 0; i < intervals.length; i++) {
-			g = intervals[i];
-			if (g.username == username) {
-				Meteor.clearInterval(g.interval);
-				intervals.splice(i,1);
-			}
-		}
+		Meteor.clearInterval(interval);
 	},
 	newGame: () => {
 		var username = Meteor.user() && Meteor.user().username;
@@ -102,13 +98,6 @@ Meteor.methods({
 		DeadPixels.remove({username: username});
 		smakes = Smakes.find({username: username}).fetch();
 		pixels = Pixels.find({username: username}).fetch();
-		for (i = 0; i < intervals.length; i++) {
-			g = intervals[i];
-			if (g.username == username) {
-				Meteor.clearInterval(g.interval);
-				intervals.splice(i,1);
-			}
-		}
 		if (smakes.length > 0) {
 			smakes.each((d) => { Smakes.remove(d._id); });
 		}
@@ -132,117 +121,121 @@ Meteor.methods({
 		Snakes[username] = makeArray(2,1,undefined);
 		Snakes[username][0] = [{x: smake.x, y: smake.y}];
 		Snakes[username][1] = [{x: opponent.x, y: opponent.y}];
-		interval = Meteor.setInterval(() => {
-				Meteor.call("tick",username)
-			},SPEED);
-		game = {
-			username: username,
-			interval: interval
-		};
-		intervals.push(game);
+    if (players == 0) {
+  		interval = Meteor.setInterval(() => {
+  				Meteor.call("tick",username)
+  			},SPEED);
+    }
+    players++;
 	},
-	tick: (username) => {
+	tick: () => {
     var PixelsRaw = Pixels.rawCollection().initializeUnorderedBulkOp();
     PixelsRaw.executeAsync = Meteor.wrapAsync(PixelsRaw.execute);
 
     var DeadPixelsRaw = DeadPixels.rawCollection().initializeUnorderedBulkOp();
     DeadPixelsRaw.executeAsync = Meteor.wrapAsync(DeadPixelsRaw.execute);
 
-		for (i = 0; i < Heads[username].length; i++) {
-      var dying = false;
-			var d = Heads[username][i];
-			var dx = +new Date();
-			dx -= d.createdAt;
-			dx = (SPEED * d.speed) / dx;
-			d.tick++;
-			if (d.tick >= d.speed) {
-				d.tick = 0;
-        if (d.speed == 0.5) doubleSpeed = 2;
-        else doubleSpeed = 1;
-        for (y = 0; y < doubleSpeed; y++) {
-          d.size += 1;
-  				var newx = d.x + Math.round(d.dx * dx);
-  				while (newx >= SIZE) newx -= SIZE;
-  				while (newx < 0) newx += SIZE;
-  				var newy = d.y + Math.round(d.dy * dx);
-  				while (newy >= SIZE) newy -= SIZE;
-  				while (newy < 0) newy += SIZE;
-  				var n = {
-  					smake: d._id,
-  					username: username,
-  					color: d.color,
-  					speed: d.speed,
-  					bot: d.bot,
-  					x: 0,
-  					y: 0,
-  					dx: +d.dx,
-  					dy: +d.dy,
-  					createdAt: +new Date()
-  				};
-  				d.x += d.dx;
-  				if (d.x >= SIZE) d.x -= SIZE;
-  				else if (d.x < 0) d.x += SIZE;
-  				d.y += d.dy;
-  				if (d.y >= SIZE) d.y -= SIZE;
-  				else if (d.y < 0) d.y += SIZE;
-  				var hit = Pix[username][d.x][d.y];
-  				if (hit != undefined) {
-            dying = true;
-  					for (k = 0; k < Snakes[username][i].length; k++) {
-  						var died = Snakes[username][i][k];
-  						var j = {
-  							username: username,
-  							smake: d._id,
-  							x: died.x,
-  							y: died.y,
-  							createdAt: +new Date()
-  						};
-  						Pix[username][died.x][died.y] = undefined;
-  					  DeadPixelsRaw.insert(j);
-  					}
-            PixelsRaw.find({username: username, smake: d._id}).removeOne();
-            DeadPixelsRaw.insert(n);
-  					d.x = Math.floor(Math.random() * SIZE);
-  					d.y = Math.floor(Math.random() * SIZE);
-  					d.dx = 1;
-  					d.dy = 0;
-  					d.length = 10;
-  					d.size = 1;
-            console.log(i + ": " + d.x + ", " + d.y);
-  					Snakes[username][i] = [{x: d.x, y: d.y}];
-  				}
-  				n.x = d.x;
-  				n.y = d.y;
-  				Pix[username][d.x][d.y] = i;
-          if (!Snakes[username][i]) Snakes[username][i] = new Array(0);
-  				Snakes[username][i].push({x: d.x,y: d.y});
-  				d.createdAt = +new Date();
-  				n.createdAt = d.createdAt;
-  				if (d.size > d.length) {
-  					var pos = Snakes[username][i][0];
-  					pixel = Pixels.findOne({username: username, smake: d._id, x: pos.x, y: pos.y},{sort: {createdAt: 1}});
-  					if (pixel) {
-  						PixelsRaw.find({_id: pixel._id}).removeOne();
-  						delete pixel._id;
-              delete pixel._collection;
-  						DeadPixelsRaw.insert(pixel);
-  					}
-  					Pix[username][pos.x][pos.y] = undefined;
-            if (Snakes[username][i] == undefined) Snakes[username][i] = new Array(0).push({x: d.x, y: d.y});
-  					if (Snakes[username][i].length > 1) Snakes[username][i].shift();
-  					if (Snakes[username][i] == undefined) Snakes[username][i] = new Array(0).push({x: d.x, y: d.y});
-  					d.size--;
-  				}
-  				Smakes.update(d._id, {$set: d});
-  				Heads[username][i] = d;
-  				PixelsRaw.insert(n);
-          if (dying) DeadPixelsRaw.insert(n);
-  			}
-  			Heads[username][i] = d;
-      }
-		}
+    var updateDead = false;
+
+    for (var username in Heads) {
+  		for (i = 0; i < Heads[username].length; i++) {
+        var dying = false;
+  			var d = Heads[username][i];
+  			var dx = +new Date();
+  			dx -= d.createdAt;
+  			dx = (SPEED * d.speed) / dx;
+  			d.tick++;
+  			if (d.tick >= d.speed) {
+  				d.tick = 0;
+          if (d.speed == 0.5) doubleSpeed = 2;
+          else doubleSpeed = 1;
+          for (y = 0; y < doubleSpeed; y++) {
+            d.size += 1;
+    				var newx = d.x + Math.round(d.dx * dx);
+    				while (newx >= SIZE) newx -= SIZE;
+    				while (newx < 0) newx += SIZE;
+    				var newy = d.y + Math.round(d.dy * dx);
+    				while (newy >= SIZE) newy -= SIZE;
+    				while (newy < 0) newy += SIZE;
+    				var n = {
+    					smake: d._id,
+    					username: username,
+    					color: d.color,
+    					speed: d.speed,
+    					bot: d.bot,
+    					x: 0,
+    					y: 0,
+    					dx: +d.dx,
+    					dy: +d.dy,
+    					createdAt: +new Date()
+    				};
+    				d.x += d.dx;
+    				if (d.x >= SIZE) d.x -= SIZE;
+    				else if (d.x < 0) d.x += SIZE;
+    				d.y += d.dy;
+    				if (d.y >= SIZE) d.y -= SIZE;
+    				else if (d.y < 0) d.y += SIZE;
+    				var hit = Pix[username][d.x][d.y];
+    				if (hit != undefined) {
+              dying = true;
+    					for (k = 0; k < Snakes[username][i].length; k++) {
+    						var died = Snakes[username][i][k];
+    						var j = {
+    							username: username,
+    							smake: d._id,
+    							x: died.x,
+    							y: died.y,
+    							createdAt: +new Date()
+    						};
+    						Pix[username][died.x][died.y] = undefined;
+    					  DeadPixelsRaw.insert(j);
+                updateDead = true;
+    					}
+              PixelsRaw.find({username: username, smake: d._id}).removeOne();
+              DeadPixelsRaw.insert(n);
+    					d.x = Math.floor(Math.random() * SIZE);
+    					d.y = Math.floor(Math.random() * SIZE);
+    					d.dx = 1;
+    					d.dy = 0;
+    					d.length = 10;
+    					d.size = 1;
+              console.log(i + ": " + d.x + ", " + d.y);
+    					Snakes[username][i] = [{x: d.x, y: d.y}];
+    				}
+    				n.x = d.x;
+    				n.y = d.y;
+    				Pix[username][d.x][d.y] = i;
+            if (!Snakes[username][i]) Snakes[username][i] = new Array(0);
+    				Snakes[username][i].push({x: d.x,y: d.y});
+    				d.createdAt = +new Date();
+    				n.createdAt = d.createdAt;
+    				if (d.size > d.length) {
+    					var pos = Snakes[username][i][0];
+    					pixel = Pixels.findOne({username: username, smake: d._id, x: pos.x, y: pos.y},{sort: {createdAt: 1}});
+    					if (pixel) {
+    						PixelsRaw.find({_id: pixel._id}).removeOne();
+    						delete pixel._id;
+                delete pixel._collection;
+    						DeadPixelsRaw.insert(pixel);
+                updateDead = true;
+    					}
+    					Pix[username][pos.x][pos.y] = undefined;
+              if (Snakes[username][i] == undefined) Snakes[username][i] = new Array(0).push({x: d.x, y: d.y});
+    					if (Snakes[username][i].length > 1) Snakes[username][i].shift();
+    					if (Snakes[username][i] == undefined) Snakes[username][i] = new Array(0).push({x: d.x, y: d.y});
+    					d.size--;
+    				}
+    				Smakes.update(d._id, {$set: d});
+    				Heads[username][i] = d;
+    				PixelsRaw.insert(n);
+            if (dying) DeadPixelsRaw.insert(n);
+    			}
+    			Heads[username][i] = d;
+        }
+  		}
+    }
     PixelsRaw.executeAsync();
-    DeadPixelsRaw.executeAsync();
+    if (updateDead) DeadPixelsRaw.executeAsync();
 	},
 	removePixel: (id, time) => {
 		DeadPixels.remove({smake: id, createdAt: {$lte: time}});
